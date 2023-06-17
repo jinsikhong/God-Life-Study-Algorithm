c5e3730a184e3279ab4f002f7a9f978a8ce29f18
wjs5025 <wjs_5025@naver.com>
2023-06-17 20:25:45 +0900

    add : gitignore

---
+++ wjs5025/BOJ_14658.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_14658 {
+    static int N, M, L, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        L = Integer.parseInt(input[2]);
+        K = Integer.parseInt(input[3]);
+
+    }
+}
