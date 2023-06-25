d03d0ddfafdc025ee858e550b6c79dc29046b413
wjs_5025 <wjs_5025@naver.com>
2023-06-26 01:05:22 +0900

    물통 / X

--- wjs5025/BOJ_14867.java
+++ wjs5025/BOJ_14867.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 
+// 접근을 못하겠습니다.
+// https://imnotabear.tistory.com/448 추후 참고용
+
 public class BOJ_14867 {
     static int a, b, c, d;
     public static void main(String[] args) throws IOException{
@@ -12,6 +15,5 @@ public class BOJ_14867 {
         c = Integer.parseInt(input[2]);
         d = Integer.parseInt(input[3]);
 
-        
     }
 }
