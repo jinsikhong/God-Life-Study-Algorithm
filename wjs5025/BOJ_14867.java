a0dbf5f61126255ec28396dcd51b27cf89b81d9f
wjs_5025 <wjs_5025@naver.com>
2023-06-26 01:05:13 +0900

    물통 / X

---
+++ wjs5025/BOJ_14867.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_14867 {
+    static int a, b, c, d;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        a = Integer.parseInt(input[0]);
+        b = Integer.parseInt(input[1]);
+        c = Integer.parseInt(input[2]);
+        d = Integer.parseInt(input[3]);
+
+        
+    }
+}
